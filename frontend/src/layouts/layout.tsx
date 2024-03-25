f76ab250e8f8dd3c607504fc4d092a3ac675488f
Aivaras Jastrumskis <aivarasj12@gmail.com>
2024-03-25 17:15:10 +0200

    responsive landing page

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 type props = {
   children: React.ReactNode;
@@ -10,8 +12,9 @@ const Layout = ({ children }: props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
   );
 };
