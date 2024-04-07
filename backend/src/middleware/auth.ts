c0b93954d844f68546673ede7cfeb8c5289dca68
Aivaras Jastrumskis <aivarasj12@gmail.com>
2024-04-07 23:11:32 +0300

    Update user profile api

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,5 +1,16 @@
 import { auth } from "express-oauth2-jwt-bearer";
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
@@ -19,4 +30,22 @@ export const jwtParse = async (
   }
 
   const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id =decoded.sub;
+
+    const user = await User.findOne({ auth0Id });
+
+    if (!user) {
+      return res.sendStatus(401)
+    }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+
+  } catch (error) {
+    return res.sendStatus(401);
+  }
 };
